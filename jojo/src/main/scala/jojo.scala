0bf8c5220d547cbca2373245eab83e40e3786e09
evoituriez <edouard.voituriez@gmail.com>
2018-03-16 18:13:59 +0100

    Ajout des typages

--- jojo/src/main/scala/jojo.scala
+++ jojo/src/main/scala/jojo.scala
@@ -1,34 +1,34 @@
 class Chien(val nom: String, var dresse:Boolean = false) {
-  def seDeplacer = "Je marche"
-  def dresser = {
+  def seDeplacer: String = "Je marche"
+  def dresser: String = {
     dresse = true
     "Je m'appelle" + this.nom + "et j'ai été dressé"
   }
-  def afficher = println("Je suis un chien, je m'appelle " + nom + " et " + seDeplacer)
+  def afficher: Unit = println("Je suis un chien, je m'appelle " + nom + " et " + seDeplacer)
 }
 
 class Serpent(val nom: String, var dresse:Boolean = false) {
-  def seDeplacer = "Je glisse"
-  def dresser = {
+  def seDeplacer: String = "Je glisse"
+  def dresser: String = {
     dresse = true
     "Je m'appelle" + this.nom + "et j'ai été dressé"
   }
-  def afficher = println("Je suis un serpent, je m'appelle " + nom + " et " + seDeplacer)
+  def afficher: Unit = println("Je suis un serpent, je m'appelle " + nom + " et " + seDeplacer)
 }
 
 class Poisson(val nom: String, var dresse:Boolean = false) {
-  def seDeplacer = "Je nage"
-  def dresser = {
+  def seDeplacer: String = "Je nage"
+  def dresser: String = {
     dresse = true
     "Je m'appelle" + this.nom + "et j'ai été dressé"
   }
-  def afficher = println("Je suis un poisson, je m'appelle " + nom + " et " + seDeplacer)
+  def afficher: Unit = println("Je suis un poisson, je m'appelle " + nom + " et " + seDeplacer)
 }
 
 object Jojo {
-  def dresser(a: Chien) = a.dresser
-  def dresser(a: Serpent) = a.dresser
-  def dresser(a: Poisson) = a.dresser
+  def dresser(a: Chien): String = a.dresser
+  def dresser(a: Serpent): String = a.dresser
+  def dresser(a: Poisson): String = a.dresser
 
   def main(args: Array[String]) = {
     val chien = new Chien("Bobby")
@@ -39,11 +39,9 @@ object Jojo {
     Jojo.dresser(serpent)
     serpent.afficher
 
-    type Multipliable = {def *(n: Double) : Double}
+    type Multipliable = {def *(n: Double): Double}
     def fois4(x: Multipliable) = x * 4
     if (3.5 .isInstanceOf[Multipliable]) fois4(3.5 .asInstanceOf[Multipliable])
   }
 }
-
-
-
+"Hello world"
