c20bc44242a9ab0bbe50bbeefcb56c09fe20cfd8
evoituriez <edouard.voituriez@gmail.com>
2018-03-16 18:50:17 +0100

    Héritage de la classe Animal

--- jojo/src/main/scala/jojo.scala
+++ jojo/src/main/scala/jojo.scala
@@ -1,47 +1,38 @@
-class Chien(val nom: String, var dresse:Boolean = false) {
-  def seDeplacer: String = "Je marche"
+class Animal(val nom: String, var dresse:Boolean = false) {
   def dresser: String = {
     dresse = true
     "Je m'appelle" + this.nom + "et j'ai été dressé"
   }
-  def afficher: Unit = println("Je suis un chien, je m'appelle " + nom + " et " + seDeplacer)
+  def afficher: Unit = println("Je suis un" this.nom ", je m'appelle " + nom)
 }
 
-class Serpent(val nom: String, var dresse:Boolean = false) {
+class Chien(n: String, d: Boolean) extends Animal(n, d) {
+  def seDeplacer: String = "Je marcher"
+}
+
+class Serpent(n: String, d: Boolean) extends Animal(n, d) {
   def seDeplacer: String = "Je glisse"
-  def dresser: String = {
-    dresse = true
-    "Je m'appelle" + this.nom + "et j'ai été dressé"
-  }
-  def afficher: Unit = println("Je suis un serpent, je m'appelle " + nom + " et " + seDeplacer)
+
 }
 
-class Poisson(val nom: String, var dresse:Boolean = false) {
+class Poisson(n: String, d: Boolean) extends Animal(n, d) {
   def seDeplacer: String = "Je nage"
-  def dresser: String = {
-    dresse = true
-    "Je m'appelle" + this.nom + "et j'ai été dressé"
-  }
-  def afficher: Unit = println("Je suis un poisson, je m'appelle " + nom + " et " + seDeplacer)
 }
 
 object Jojo {
-  def dresser(a: Chien): String = a.dresser
-  def dresser(a: Serpent): String = a.dresser
-  def dresser(a: Poisson): String = a.dresser
+  def dresser(a: Animal): String = a.dresser
 
-  def main(args: Array[String]) = {
-    val chien = new Chien("Bobby")
-    val serpent = new Serpent("Tika")
-    val poisson = new Poisson("Gustave")
-    chien.afficher
-    poisson.afficher
+  val serpent = new Serpent("Garry")
+  val poisson = new Poisson("Mike")
+  val chien = new Chien("Kim")
+
+  type Dressable = {def dresser() = Unit}
+  def dresser(a: Dressable) = a.dresser
+
+  if (serpent.isInstanceOf[Jojo.Dressable])
     Jojo.dresser(serpent)
-    serpent.afficher
-
-    type Multipliable = {def *(n: Double): Double}
-    def fois4(x: Multipliable) = x * 4
-    if (3.5 .isInstanceOf[Multipliable]) fois4(3.5 .asInstanceOf[Multipliable])
-  }
 }
-"Hello world"
+
+object Main extends App {
+
+}
