04601e1d3a45e1a5181811bc5eecbed2bdaf9f57
evoituriez <edouard.voituriez@gmail.com>
2018-03-16 19:08:17 +0100

    Correction héritage

--- jojo/src/main/scala/jojo.scala
+++ jojo/src/main/scala/jojo.scala
@@ -1,9 +1,9 @@
-class Animal(val nom: String, var dresse:Boolean = false) {
+class Animal(val nom: String, var dresse: Boolean = false) {
   def dresser: String = {
     dresse = true
     "Je m'appelle" + this.nom + "et j'ai été dressé"
   }
-  def afficher: Unit = println("Je suis un" this.nom ", je m'appelle " + nom)
+  def afficher: Unit = println("Je suis un animal, je m'appelle " + nom)
 }
 
 class Chien(n: String, d: Boolean) extends Animal(n, d) {
@@ -12,7 +12,6 @@ class Chien(n: String, d: Boolean) extends Animal(n, d) {
 
 class Serpent(n: String, d: Boolean) extends Animal(n, d) {
   def seDeplacer: String = "Je glisse"
-
 }
 
 class Poisson(n: String, d: Boolean) extends Animal(n, d) {
@@ -21,18 +20,12 @@ class Poisson(n: String, d: Boolean) extends Animal(n, d) {
 
 object Jojo {
   def dresser(a: Animal): String = a.dresser
-
-  val serpent = new Serpent("Garry")
-  val poisson = new Poisson("Mike")
-  val chien = new Chien("Kim")
-
-  type Dressable = {def dresser() = Unit}
-  def dresser(a: Dressable) = a.dresser
-
-  if (serpent.isInstanceOf[Jojo.Dressable])
-    Jojo.dresser(serpent)
 }
 
 object Main extends App {
+  val chien: Animal = new Chien("Bobby")
+  val serpent : Animal = new Serpent("Garry")
 
+  Jojo.dresser(chien)
+  Jojo.dresser(serpent)
 }
